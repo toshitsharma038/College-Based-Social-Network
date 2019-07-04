068624169fb31b091435ed743b12d3c8ae19d267
toshitsharma038 <44299217+toshitsharma038@users.noreply.github.com>
2019-07-04 11:35:41 +0530

    accounts

---
+++ accounts/models.py
@@ -0,0 +1,16 @@
+from django.contrib import auth
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+
+class User(auth.models.User, auth.models.PermissionsMixin):
+
+    def __str__(self):
+        return "@{}".format(self.username)
+
+class UserSignup(models.Model):
+    collegeid = models.CharField(max_length=100,unique=True)
+#    portfolio_site = models.URLField(blank=True)
+
+    def __str__(self):
+        return self.collegeid
---
+++ accounts/views.py
@@ -0,0 +1,15 @@
+from django.contrib.auth import login, logout
+from django.core.urlresolvers import reverse_lazy
+from django.views.generic import CreateView
+
+from . import forms
+
+class SignUp(CreateView):
+    form_class = forms.UserCreateForm
+    success_url = reverse_lazy("login")
+    template_name = "accounts/signup.html"
+
+class Forget(CreateView):
+    form_class = forms.UserCreateForm
+    success_url = reverse_lazy("login")
+    template_name = "accounts/forget.html"
